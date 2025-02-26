173a32a3d3e36fae4e83f9be73cbb04c71cc6ad1
icmor <icornejomora@gmail.com>
2025-02-25 18:56:06 -0600

    Cambios mínimos para claridad

--- ruta_euleriana.py
+++ ruta_euleriana.py
@@ -21,6 +21,7 @@ def get_max_degree(matrix: adjacency_matrix) -> (int, int):
             max_vertex = vertex
     return max_vertex, max_degree
 
+
 def buscar_camino_euleriano(matriz_adyacencia):
     """Usa el algoritmo de Hierholzer para encontrar un camino euleriano en la grafica.
 
@@ -43,24 +44,27 @@ def buscar_camino_euleriano(matriz_adyacencia):
             else:
                 return None
 
-    # Si hay dos vértices impares, añadimos una arista ficticia para cerrar el ciclo
-    if inicio != -1:
-        matriz_adyacencia[inicio][fin] += 1
-        matriz_adyacencia[fin][inicio] += 1
-    else:
+    # Si no hay vertices elegimos cualquier vértice no aislado
+    if inicio == -1:
         # Busca el primer vértice no aislado para empezar
-        inicio = next((i for i, grado in enumerate(grados) if grado > 0), -1)
+        for vertice, grado in enumerate(grados):
+            if grado > 0:
+                inicio = vertice
         if inicio == -1:
             return []
 
-    pila, camino = [inicio], []
+    pila = [inicio]
+    camino = []
 
     # Procesa cada vértice usando un recorrido en profundidad modificado
     while pila:
         vertice_actual = pila[-1]
 
         # Buscar una arista no utilizada
-        siguiente = next((i for i, valor in enumerate(matriz_adyacencia[vertice_actual]) if valor > 0), None)
+        siguiente = None
+        for vecino, arista in enumerate(matriz_adyacencia[vertice_actual]):
+            if arista == 1:
+                siguiente = vecino
 
         if siguiente is None:
             # Añadir al camino si ya no tiene aristas
@@ -68,22 +72,15 @@ def buscar_camino_euleriano(matriz_adyacencia):
             pila.pop()
         else:
             # Usar arista y continuar al siguiente vértice
-            matriz_adyacencia[vertice_actual][siguiente] -= 1
-            matriz_adyacencia[siguiente][vertice_actual] -= 1
+            matriz_adyacencia[vertice_actual][siguiente] = 0
+            matriz_adyacencia[siguiente][vertice_actual] = 0
             pila.append(siguiente)
-
-    if fin != -1:
-        # Ajustar el camino para eliminar la arista ficticia
-        for i in range(len(camino) - 1):
-            if (camino[i] == inicio and camino[i + 1] == fin) or (camino[i] == fin and camino[i + 1] == inicio):
-                camino = camino[i + 1:] + camino[1:i + 1]
-                break
 
     # Verifique si quedan aristas, lo que indicaría que no hay camino euleriano debido a la falta de conexidad
     if any(sum(fila) > 0 for fila in matriz_adyacencia):
         return None
-
     return camino
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
